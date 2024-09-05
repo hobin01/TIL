afa55dbf3e04fd2b9566a459b7a19e92e947d21c
hobin01 <ghqls0210@naver.com>
2024-09-05 23:14:54 +0900

    stack

---
+++ 2024/cpp_no_stl/data_structure/stack.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+
+using namespace std;
+
+// stack : push, pop, empty, size, clear, top
+
+template <typename T>
+struct Node {
+    T data;
+    Node* next;
+};
+
+template <typename T>
+class Stack {
+private:
+    Node<T>* _top;
+    size_t _size;
+
+public:
+    Stack() : _top(nullptr), _size(0) {}
+    ~Stack() {}
+
+    void push(T data) {
+        Node<T>* newNode = new Node<T>();
+        newNode->data = data;
+        newNode->next = _top;
+        _top = newNode;
+        ++_size;
+    }
+
+    T pop() {
+        if(empty()) {
+            cout << "pop error : Stack is empty!\n";
+            return T();
+        }
+
+        T data = _top->data;
+        Node<T>* tmp = _top;
+        _top = _top->next;
+        delete tmp;
+        --_size;
+
+        return data;
+    }
+
+    bool empty() {
+        return _top == nullptr;
+    }
+
+    size_t size() {
+        return _size;
+    }
+
+    T top() {
+        if(empty()) {
+            cout << "top error : Stack is empty!\n";
+            return T();
+        }
+
+        return _top->data;
+    }
+
+    void clear() {
+        while(!empty()) {
+            pop();
+        }
+        _top = nullptr;
+        _size = 0;
+    }
+};
+
+int main() {
+    Stack<int> stk;
+    stk.push(1);
+    stk.push(2);
+
+    cout << "size : " << stk.size() << '\n';
+    cout << "top : " << stk.top() << '\n';
+
+    stk.pop();
+
+    cout << "size : " << stk.size() << '\n';
+    cout << "top : " << stk.top() << '\n';
+
+    stk.pop();
+
+    cout << "size : " << stk.size() << '\n';
+    cout << "top : " << stk.top() << '\n';
+
+    stk.pop();
+
+    cout << "size : " << stk.size() << '\n';
+    cout << "top : " << stk.top() << '\n';
+
+    stk.push(4);
+    stk.push(5);
+
+    cout << "size : " << stk.size() << '\n';
+    cout << "top : " << stk.top() << '\n';
+
+    stk.pop();
+    stk.push(6);
+
+    cout << "size : " << stk.size() << '\n';
+    cout << "top : " << stk.top() << '\n';
+
+    stk.clear();
+
+    cout << "size : " << stk.size() << '\n';
+    cout << "top : " << stk.top() << '\n';
+
+    stk.pop();
+
+    cout << "size : " << stk.size() << '\n';
+    cout << "top : " << stk.top() << '\n';
+}
