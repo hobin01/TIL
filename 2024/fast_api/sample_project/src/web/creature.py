b0ecbaa7cccc9ac5a840f0c042e59b66a72a7b38
hobin01 <ghqls0210@naver.com>
2024-09-18 23:21:46 +0900

    chapter 10

--- 2024/fast_api/sample_project/src/web/creature.py
+++ 2024/fast_api/sample_project/src/web/creature.py
@@ -1,30 +1,51 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from model.creature import Creature
 import service.creature as service
+from error import Missing, Duplicate
 
 router = APIRouter(prefix="/creature")
 
+@router.get("")
 @router.get("/")
 def get_all() -> list[Creature]:
     return service.get_all()
 
 @router.get("/{name}")
-def get_one(name) -> Creature | None:
-    return service.get_one(name)
+@router.get("/{name}/")
+def get_one(name: str) -> Creature:
+    try : 
+        return service.get_one(name)
+    except Missing as exc :
+        raise HTTPException(status_code=404, detail=exec.msg)
 
-# 나머지 엔드포인트 (현재는 아무 기능 없음)
-@router.post("/")
+@router.post("", status_code=201)
+@router.post("/", status_code=201)
 def create(creature: Creature) -> Creature:
-    return service.create(creature)
+    try : 
+        return service.create(creature)
+    except Duplicate as exc :
+        raise HTTPException(status_code=404, detail=exc.msg)
 
 @router.patch("/{name}")
-def modify(name, creature: Creature) -> Creature:
-    return service.modify(name, creature)
+@router.patch("/{name}/")
+def modify(name: str, creature: Creature) -> Creature:
+    try : 
+        return service.modify(name, creature)
+    except Missing as exc :
+        raise HTTPException(status_code=404, detail=exc.msg)
 
 @router.put("/{name}")
+@router.put("/{name}/")
 def replace(name, creature: Creature) -> Creature:
-    return service.replace(name, creature)
+    try : 
+        return service.replace(name, creature)
+    except Missing as exc :
+        raise HTTPException(status_code=404, detail=exc.msg)
 
 @router.delete("/{name}")
+@router.delete("/{name}/")
 def delete(name) -> bool:
-    return service.delete(name)
+    try : 
+        return service.delete(name)
+    except Missing as exc : 
+        raise HTTPException(status_code=404, detail=exc.msg)
