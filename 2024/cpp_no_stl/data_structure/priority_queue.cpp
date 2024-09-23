6f99ed7c42153077bdb5c6986f38ac3a2bf20492
hobin01 <ghqls0210@naver.com>
2024-09-23 22:56:05 +0900

    priority queue

--- 2024/cpp_no_stl/data_structure/priority_queue.cpp
+++ 2024/cpp_no_stl/data_structure/priority_queue.cpp
@@ -54,7 +54,7 @@ private:
 
     // 다 찬 경우, cap 2배로 증가
     void resize() {
-        int* newHeap = new int[_cap * 2];
+        T* newHeap = new T[_cap * 2];
 
         for(int i = 0; i < _size; i++)
             newHeap[i] = _heap[i];
@@ -65,9 +65,98 @@ private:
     }
 
 public:
-}
+    // 초기 cap 10으로 지정
+    PriorityQueue() : _size(0), _cap(10) {
+        _heap = new T[_cap];
+    }
+
+    // pq 해제 시 할당했던 메모리 전체 해제
+    ~PriorityQueue() {
+        delete[] _heap;
+    }
+
+    // pq 내 모든 원소 삭제
+    void clear() {
+        delete[] _heap;
+        _size = 0;
+        _cap = 10;
+
+        _heap = new T[_cap];
+    }
+
+    // 원소 삽입 (최대 힙)
+    void push(T val) {
+        if(_size == _cap) {
+            resize();
+        }
+        _heap[_size] = val;
+        heapifyUp(_size);
+        _size++;
+    }
+
+    // 원소 삭제 (top 제거)
+    T pop() {
+        if(_size == 0) {
+            cout << "PQ is empty!!!\n";
+            return T();
+        }
+        
+        int maxVal = _heap[0];
+        _heap[0] = _heap[_size - 1];
+        _size--;
+        heapifyDown(0);
+
+        return maxVal;
+    }
+
+    // 최대값 반환 
+    T top() {
+        if(_size == 0) {
+            cout << "PQ is empty!!!\n";
+            return T();
+        }
+        
+        return _heap[0];
+    }
+
+    // 큐 비었는 지 확인 
+    bool empty() {
+        return _size == 0;
+    }
+
+    // 큐 크기 확인 
+    size_t size() {
+        return _size;
+    }
+};
 
 int main()
 {
+    PriorityQueue<int> pq;
+    pq.push(10);
+    pq.push(20);
+    pq.push(5);
+    pq.push(100);
 
+    cout << "top : " << pq.top() << '\n';
+    pq.pop();
+    cout << "top : " << pq.top() << '\n';
+    pq.pop();
+
+    cout << "size : " << pq.size() << '\n';
+    cout << "empty : " << pq.empty() << '\n';
+
+    pq.clear();
+    cout << "size : " << pq.size() << '\n';
+    cout << "empty : " << pq.empty() << '\n';
+
+    pq.pop();
+
+    pq.push(100);
+    pq.push(-123);
+    cout << "top : " << pq.top() << '\n';
+    pq.pop();
+    cout << "top : " << pq.top() << '\n';
+    pq.pop();
+    cout << "top : " << pq.top() << '\n';
 }
